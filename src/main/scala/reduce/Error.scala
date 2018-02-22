7f856e82a837a57f93d0943c18e525230b136425
ian <ianmccall93@gmail.com>
2018-02-21 17:11:30 -0800

    Added error for application of non applicable type

--- src/main/scala/reduce/Error.scala
+++ src/main/scala/reduce/Error.scala
@@ -1,7 +1,10 @@
 package reduce
 
+import ast.Type
+
 sealed trait Error
 
+case class ApplicationOfNonAppliableType(t: Type) extends Error
 case class RecursiveVariableDef() extends Error
 case class UnknownName(n: String) extends Error
 case class WrongNumArgs() extends Error
