5ac76466f64300e08efdd5f8c5ff02202d122d7e
ian <ianmccall93@gmail.com>
2018-02-22 09:35:45 -0800

    Made errors and types case objects for nicer pretty printing

--- src/main/scala/reduce/Error.scala
+++ src/main/scala/reduce/Error.scala
@@ -5,7 +5,7 @@ import ast.Type
 sealed trait Error
 
 case class ApplicationOfNonAppliableType(t: Type) extends Error
-object RecursiveVariableDef extends Error
+case object RecursiveVariableDef extends Error
 case class UnknownName(n: String) extends Error
 case class WrongNumArgs(expected: Int, found: Int) extends Error
 
