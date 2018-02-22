3f574491accda7202b26ed8d32e380a9ebfaabbf
ian <ianmccall93@gmail.com>
2018-02-21 17:11:47 -0800

    Added the number of args to error WrongNumArgs

--- src/main/scala/reduce/Error.scala
+++ src/main/scala/reduce/Error.scala
@@ -7,5 +7,5 @@ sealed trait Error
 case class ApplicationOfNonAppliableType(t: Type) extends Error
 case class RecursiveVariableDef() extends Error
 case class UnknownName(n: String) extends Error
-case class WrongNumArgs() extends Error
+case class WrongNumArgs(expected: Int, found: Int) extends Error
 
