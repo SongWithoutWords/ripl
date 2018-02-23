e7412d5871bfc5b495d148c3360fe477f58af1fa
ian <ianmccall93@gmail.com>
2018-02-23 09:43:05 -0800

    Added TypeConflict error

--- src/main/scala/reduce/Error.scala
+++ src/main/scala/reduce/Error.scala
@@ -6,6 +6,7 @@ sealed trait Error
 
 case class ApplicationOfNonAppliableType(t: Type) extends Error
 case class RecursiveVariableDef(n: Node) extends Error
+case class TypeConflict(expected: Type, found: Type) extends Error
 case class UnknownName(n: String) extends Error
 case class WrongNumArgs(expected: Int, found: Int) extends Error
 
