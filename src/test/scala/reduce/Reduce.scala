bb9890e82d548df120c7ab6f7715bf34438ecc0a
ian <ianmccall93@gmail.com>
2018-04-14 15:35:23 -0700

    Uncommented floating point overload test

--- src/test/scala/reduce/Reduce.scala
+++ src/test/scala/reduce/Reduce.scala
@@ -368,16 +368,15 @@ class TestReduce extends FreeSpec with Matchers {
                  a1.Name("b", a1.Param("b", TInt)))))()
     }
     "floating point addition is selected for floats" in {
-      ???
-      // test(
-      //   "iAdd" -> a0.Fun(a0.Param("a", TFlt), a0.Param("b", TFlt))(Some(TFlt))(
-      //     a0.App(a0.Name("+"),
-      //            a0.Name("a"),
-      //            a0.Name("b"))))(
-      //   "iAdd" -> a1.Fun(a1.Param("a", TFlt), a1.Param("b", TFlt))(TFlt)(
-      //     a1.App(a1.Intrinsic.FAdd,
-      //            a1.Name("a", a1.Param("a", TFlt)),
-      //            a1.Name("b", a1.Param("b", TFlt)))))()
+      test(
+        "iAdd" -> a0.Fun(a0.Param("a", TFlt), a0.Param("b", TFlt))(Some(TFlt))(
+          a0.App(a0.Name("+"),
+                 a0.Name("a"),
+                 a0.Name("b"))))(
+        "iAdd" -> a1.Fun(a1.Param("a", TFlt), a1.Param("b", TFlt))(TFlt)(
+          a1.App(a1.Intrinsic.FAdd,
+                 a1.Name("a", a1.Param("a", TFlt)),
+                 a1.Name("b", a1.Param("b", TFlt)))))()
     }
   }
 }
