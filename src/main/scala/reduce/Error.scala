17471769abc55ed86188df837e0e1dd5a56037cb
ian <ianmccall93@gmail.com>
2018-05-23 14:35:18 -0700

    Error for ambiguous implicit conversion

--- src/main/scala/reduce/Error.scala
+++ src/main/scala/reduce/Error.scala
@@ -5,8 +5,9 @@ import reduce.ast.{typed => a1}
 
 sealed trait Error
 
-case class AmbiguousType(input: a0.Node) extends Error
+case class AmbiguousImplicitConversions(conversions: List[a1.Exp]) extends Error
 case class AmbiguousOverload[A](overloads: List[ReduceM[A]]) extends Error
+case class AmbiguousType(input: a0.Node) extends Error
 case class AmbiguousUnit(overloads: List[ReduceM[a1.Node]]) extends Error
 case class ApplicationOfNonAppliableType(t: a1.Type) extends Error
 case class NonExistentMember(n: String) extends Error
@@ -14,7 +15,7 @@ case class RecursiveVariableDef(cycle: List[a0.Node]) extends Error
 case class RequiredExp(found: a1.Node) extends Error
 case class RequiredType(input: a0.Node) extends Error
 case class SelectionFromNonStructType(t: a1.Type) extends Error
-case class TypeConflict(expected: a1.Type, found: a1.Type) extends Error
+case class TypeConflict(expected: a1.Type, found: a1.Type) extends Error // Should be no-implicitConversion?
 case class UnknownName(n: String) extends Error
 case object UseOfInvalidExp extends Error
 case class WrongNumArgs(expected: Int, found: Int) extends Error
