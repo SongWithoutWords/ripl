9ff2f50044ca40d9a5cc7a67a82b78191b917641
ian <ianmccall93@gmail.com>
2018-05-22 22:07:13 -0700

    Enforce known return types from functions

--- src/main/scala/reduce/Reduce.scala
+++ src/main/scala/reduce/Reduce.scala
@@ -300,9 +300,10 @@ class Reduce(val astIn: a0.Ast) {
 
     case a0.Fun(_params, _retType, _body) => List(for {
       params <- mapM(_params) {p => for {t <- mapAsType(p.t)} yield a1.Param(p.n, t)}
+
       retType <- _retType match {case Some(t) => mapAsType(t); case _ => pure(TError)}
       _ <- impure(pushScope(MultiMap(params.map(p => (p.n, p)): _*)))
-      body <- mapAsExp(None, _body)
+      body <- mapAsExp(Some(retType), _body)
       _ <- impure(popScope())
     } yield a1.Fun(params, retType, body))
 
