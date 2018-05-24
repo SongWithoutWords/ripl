156a64db704fcf7619e369fcd1cda5730e95dcda
ian <ianmccall93@gmail.com>
2018-05-24 16:55:52 -0700

    ParseTreeVisitor implementations for names, booleans, ints and floats

--- src/main/scala/ripl/parse/ParseTreeVisitor.scala
+++ src/main/scala/ripl/parse/ParseTreeVisitor.scala
@@ -1,16 +1,21 @@
 package ripl.parser
 
 import ripl.parser.antlr._
+import ripl.ast.common._
 import ripl.ast.untyped._
 
 class ParseTreeVisitor extends riplBaseVisitor[Node] {
 
-  // override def visitBinaryOp(ctx: ripleParser.BinaryContext): App = {
-    // ctx.
-  // }
+  override def visitName(c: riplParser.NameContext): Name
+    = Name(c.Name().getText)
 
+  override def visitBln(c: riplParser.BlnContext): VBln
+    = VBln(c.VBln().getText.toBoolean)
 
+  override def visitInt(c: riplParser.IntContext): VInt
+    = VInt(c.VInt().getText.toInt)
 
-
+  override def visitFlt(c: riplParser.FltContext): VFlt
+    = VFlt(c.getText.toFloat)
 }
 
