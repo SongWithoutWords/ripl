48b75d90f5b4c3e796254d156a3149f061d2ebf6
ian <ianmccall93@gmail.com>
2018-05-24 15:55:14 -0700

    Additional name test cases

--- src/test/scala/ripl/parse/Parser.scala
+++ src/test/scala/ripl/parse/Parser.scala
@@ -2,6 +2,7 @@ package ripl.parser
 
 import org.scalatest._
 
+import ripl.ast.common._
 import ripl.ast.untyped._
 import ripl.parser._
 
@@ -10,23 +11,77 @@ import ripl.reduce.CustomMatchers.matchAst
 class TestParser extends FreeSpec with Matchers {
 
   def test(input: String)(out: Node): Unit = Parser(input) should matchAst(out)
+  def testName(input: String): Unit = Parser(input) should matchAst(Name(input))
 
-  "exp0" - {
-    "names" - {
-      "John is a name" in {
-        test("John")(Name("John"))
+  "expressions" - {
+    "exp0" - {
+      "names" - {
+        "John is a name" in {
+          testName("John")
+        }
+        "tim is a name" in {
+          testName("tim")
+        }
+        "H2O is a name" in {
+          testName("H2O")
+        }
+        "camelCase is a name" in {
+          testName("camelCase")
+        }
+        "StudlyCase is a name" in {
+          testName("StudlyCase")
+        }
+        "snake_case is a name" in {
+          testName("snake_case")
+        }
+        "SCREAMING_SNAKE_CASE is a name" in {
+          testName("SCREAMING_SNAKE_CASE")
+        }
+        "_surrounded_in_under_scores_ is a name" in {
+          testName("_surrounded_in_under_scores_")
+        }
+        "+ is a name" in {
+          testName("+")
+        }
+        "- is a name" in {
+          testName("-")
+        }
+        "* is a name" in {
+          testName("*")
+        }
+        "/ is a name" in {
+          testName("/")
+        }
+        "% is a name" in {
+          testName("%")
+        }
+        "++ is a name" in {
+          testName("++")
+        }
+        ">>= is a name" in {
+          testName(">>=")
+        }
+        "--< is a name" in {
+          testName("--<")
+        }
+      }
+      "integer literals" - {
+        "0 is an integer" in {
+          test("0")(VInt(0))
+        }
+        "4 is an integer" in {
+          test("4")(VInt(4))
+        }
+
+
+      }
+      "floating point literals" - {
+
+      }
+      "bracketed expressions" - {
+
       }
     }
-    "integer literals" - {
-
-    }
-    "floating point literals" - {
-
-    }
-    "bracketed expressions" - {
-
-    }
   }
-
 }
 
