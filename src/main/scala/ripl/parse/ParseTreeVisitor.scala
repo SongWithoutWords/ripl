571440b1d3c2eae5964f90a432a4381b2e284081
ian <ianmccall93@gmail.com>
2018-05-24 23:03:48 -0700

    Parse tree visitor handling for exp0, exp1, addition and multiplication

--- src/main/scala/ripl/parse/ParseTreeVisitor.scala
+++ src/main/scala/ripl/parse/ParseTreeVisitor.scala
@@ -6,6 +6,13 @@ import ripl.ast.untyped._
 
 class ParseTreeVisitor extends riplBaseVisitor[Node] {
 
+  def visitExp0(c: riplParser.Exp0Context): Exp = c match {
+    case n: riplParser.NameContext => visitName(n)
+    case n: riplParser.BlnContext => visitBln(n)
+    case n: riplParser.IntContext => visitInt(n)
+    case n: riplParser.FltContext => visitFlt(n)
+  }
+
   override def visitName(c: riplParser.NameContext): Name
     = Name(c.Name().getText)
 
@@ -17,5 +24,22 @@ class ParseTreeVisitor extends riplBaseVisitor[Node] {
 
   override def visitFlt(c: riplParser.FltContext): VFlt
     = VFlt(c.getText.toFloat)
+
+
+  def visitExp1(c: riplParser.Exp1Context): Exp = c match {
+    case n: riplParser.AdditionContext => visitAddition(n)
+    case n: riplParser.MultiplicationContext => visitMultiplication(n)
+    case n: riplParser.Exp10Context => visitExp0(n.exp0)
+  }
+
+  override def visitAddition(c: riplParser.AdditionContext): App =
+    App(Name("+"),
+      visitExp1(c.exp1(0)),
+      visitExp1(c.exp1(1)))
+
+  override def visitMultiplication(c: riplParser.MultiplicationContext): App =
+    App(Name("*"),
+      visitExp1(c.exp1(0)),
+      visitExp1(c.exp1(1)))
 }
 
