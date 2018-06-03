973bc1692f86f5a8f968a72aa792d9b7b9b00710
ian <ianmccall93@gmail.com>
2018-06-02 19:12:19 -0700

    Make TypeAtoms enums with names so they can be looked up in a map

--- src/main/scala/ripl/ast/common/TypeAtom.scala
+++ src/main/scala/ripl/ast/common/TypeAtom.scala
@@ -1,13 +1,19 @@
 package ripl.ast.common
 
+import enumeratum._
+
 import ripl.ast.typed.Type
 
+sealed trait TypeAtom extends Type with EnumEntry { val n: String }
 
-sealed trait TypeAtom extends Type
-case object TBln extends TypeAtom
-case object TError extends TypeAtom
-case object TFlt extends TypeAtom
-case object TInt extends TypeAtom
-case object TNone extends TypeAtom
-case object TStr extends TypeAtom
+case object TypeAtom extends Enum[TypeAtom] {
+  val values = findValues
+
+  case object TBln extends TypeAtom { val n = "Bln" }
+  case object TFlt extends TypeAtom { val n = "Flt" }
+  case object TInt extends TypeAtom { val n = "Int" }
+  case object TNone extends TypeAtom { val n = "None" }
+  case object TStr extends TypeAtom { val n = "Str" }
+}
+
 
