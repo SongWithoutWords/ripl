58a65c5b91bdb7df2888e2bea5efa5b7368bd51a
ian <ianmccall93@gmail.com>
2018-06-04 11:35:58 -0700

    Port llvm/Global to scala

--- src/main/scala/ripl/llvm/pure/ast/Global.scala
+++ src/main/scala/ripl/llvm/pure/ast/Global.scala
@@ -1,136 +1,120 @@
--- | 'Global's - top-level values in 'Module's - and supporting structures.
-module LLVM.AST.Global where
+// 'Global's - top-level values in 'Module's - and supporting structures.
+package ripl.llvm.pure.ast
 
-import LLVM.Prelude
+// <http://llvm.org/doxygen/classllvm_1_1GlobalValue.html>
+sealed trait Global
+    // <http://llvm.org/docs/LangRef.html#global-variables>
+case class GlobalVariable(
+        name: Name,
+        linkage: L.Linkage,
+        visibility: V.Visibility,
+        dllStorageClass: Option[DLL.StorageClass],
+        threadLocalMode: Option[TLS.Model],
+        unnamedAddr: Option[UnnamedAddr],
+        isConstant: Bool,
+        t: Type,
+        addrSpace: AddrSpace,
+        initializer: Option[Constant],
+        section: Option[String],
+        comdat: Option[String],
+        alignment: Word32
+      )
+    // <http://llvm.org/docs/LangRef.html#aliases>
+case class GlobalAlias(
+        name: Name,
+        linkage: L.Linkage,
+        visibility: V.Visibility,
+        dllStorageClass: Option[DLL.StorageClass],
+        threadLocalMode: Option[TLS.Model],
+        unnamedAddr: Option[UnnamedAddr],
+        t: Type,
+        addrSpace: AddrSpace,
+        aliasee: Constant
+      )
+    // <http://llvm.org/docs/LangRef.html#functions>
+case class Function(
+        linkage: L.Linkage,
+        visibility: V.Visibility,
+        dllStorageClass: Option[DLL.StorageClass],
+        callingConvention: CC.CallingConvention,
+        returnAttributes: List[ParameterAttribute],
+        returnType: Type,
+        name: Name,
+        parameters: (List[Parameter],Bool), // ^ snd indicates varargs
+        functionAttributes: List[Either[A.GroupID, A.FunctionAttribute]],
+        section: Option[String],
+        comdat: Option[String],
+        alignment: Word32,
+        garbageCollectorName: Option[String],
+        prefix: Option[Constant],
+        basicBlocks: List[BasicBlock],
+        personalityFunction: Option[Constant]
+      )
 
-import LLVM.AST.Name
-import LLVM.AST.Type
-import LLVM.AST.Constant (Constant)
-import LLVM.AST.AddrSpace
-import LLVM.AST.Instruction (Named, Instruction, Terminator)
-import qualified LLVM.AST.Linkage as L
-import qualified LLVM.AST.Visibility as V
-import qualified LLVM.AST.DLL as DLL
-import qualified LLVM.AST.CallingConvention as CC
-import qualified LLVM.AST.ThreadLocalStorage as TLS
-import qualified LLVM.AST.Attribute as A
+// 'Parameter's for 'Function's
+case class Parameter(t: Type, name: Name, attributes: List[ParameterAttribute])
 
--- | <http://llvm.org/doxygen/classllvm_1_1GlobalValue.html>
-data Global
-    -- | <http://llvm.org/docs/LangRef.html#global-variables>
-    = GlobalVariable {
-        name :: Name,
-        linkage :: L.Linkage,
-        visibility :: V.Visibility,
-        dllStorageClass :: Maybe DLL.StorageClass,
-        threadLocalMode :: Maybe TLS.Model,
-        unnamedAddr :: Maybe UnnamedAddr,
-        isConstant :: Bool,
-        type' :: Type,
-        addrSpace :: AddrSpace,
-        initializer :: Maybe Constant,
-        section :: Maybe ShortByteString,
-        comdat :: Maybe ShortByteString,
-        alignment :: Word32
-      }
-    -- | <http://llvm.org/docs/LangRef.html#aliases>
-    | GlobalAlias {
-        name :: Name,
-        linkage :: L.Linkage,
-        visibility :: V.Visibility,
-        dllStorageClass :: Maybe DLL.StorageClass,
-        threadLocalMode :: Maybe TLS.Model,
-        unnamedAddr :: Maybe UnnamedAddr,
-        type' :: Type,
-        addrSpace :: AddrSpace,
-        aliasee :: Constant
-      }
-    -- | <http://llvm.org/docs/LangRef.html#functions>
-    | Function {
-        linkage :: L.Linkage,
-        visibility :: V.Visibility,
-        dllStorageClass :: Maybe DLL.StorageClass,
-        callingConvention :: CC.CallingConvention,
-        returnAttributes :: [A.ParameterAttribute],
-        returnType :: Type,
-        name :: Name,
-        parameters :: ([Parameter],Bool), -- ^ snd indicates varargs
-        functionAttributes :: [Either A.GroupID A.FunctionAttribute],
-        section :: Maybe ShortByteString,
-        comdat :: Maybe ShortByteString,
-        alignment :: Word32,
-        garbageCollectorName :: Maybe ShortByteString,
-        prefix :: Maybe Constant,
-        basicBlocks :: [BasicBlock],
-        personalityFunction :: Maybe Constant
-      }
-  deriving (Eq, Read, Show, Typeable, Data, Generic)
+// <http://llvm.org/doxygen/classllvm_1_1BasicBlock.html>
+// LLVM code in a function is a sequence of 'BasicBlock's each with a label,
+// some instructions, and a terminator.
+case class BasicBlock(name: Name, instructions: List[Named[Instruction]], terminator: Named[Terminator])
 
--- | 'Parameter's for 'Function's
-data Parameter = Parameter Type Name [A.ParameterAttribute]
-  deriving (Eq, Read, Show, Typeable, Data, Generic)
+sealed trait UnnamedAddr
+case object LocalAddr extends UnnamedAddr
+case object GlobalAddr extends UnnamedAddr
 
--- | <http://llvm.org/doxygen/classllvm_1_1BasicBlock.html>
--- LLVM code in a function is a sequence of 'BasicBlock's each with a label,
--- some instructions, and a terminator.
-data BasicBlock = BasicBlock Name [Named Instruction] (Named Terminator)
-  deriving (Eq, Read, Show, Typeable, Data, Generic)
+// // helper for making 'GlobalVariable's
+// globalVariableDefaults: Global
+// globalVariableDefaults = 
+//   GlobalVariable(
+//   name = error "global variable name not defined",
+//   linkage = L.External,
+//   visibility = V.Default,
+//   dllStorageClass = Nothing,
+//   threadLocalMode = Nothing,
+//   addrSpace = AddrSpace 0,
+//   unnamedAddr = Nothing,
+//   isConstant = False,
+//   t = error "global variable type not defined",
+//   initializer = Nothing,
+//   section = Nothing,
+//   comdat = Nothing,
+//   alignment = 0
+//   )
 
-data UnnamedAddr = LocalAddr | GlobalAddr
-  deriving (Eq, Read, Show, Typeable, Data, Generic)
+// // helper for making 'GlobalAlias's
+// globalAliasDefaults: Global
+// globalAliasDefaults =
+//   GlobalAlias(
+//     name = error "global alias name not defined",
+//     linkage = L.External,
+//     visibility = V.Default,
+//     dllStorageClass = Nothing,
+//     threadLocalMode = Nothing,
+//     unnamedAddr = Nothing,
+//     t = error "global alias type not defined",
+//     addrSpace = AddrSpace 0,
+//     aliasee = error "global alias aliasee not defined"
+//   )
 
--- | helper for making 'GlobalVariable's
-globalVariableDefaults :: Global
-globalVariableDefaults = 
-  GlobalVariable {
-  name = error "global variable name not defined",
-  linkage = L.External,
-  visibility = V.Default,
-  dllStorageClass = Nothing,
-  threadLocalMode = Nothing,
-  addrSpace = AddrSpace 0,
-  unnamedAddr = Nothing,
-  isConstant = False,
-  type' = error "global variable type not defined",
-  initializer = Nothing,
-  section = Nothing,
-  comdat = Nothing,
-  alignment = 0
-  }
-
--- | helper for making 'GlobalAlias's
-globalAliasDefaults :: Global
-globalAliasDefaults =
-  GlobalAlias {
-    name = error "global alias name not defined",
-    linkage = L.External,
-    visibility = V.Default,
-    dllStorageClass = Nothing,
-    threadLocalMode = Nothing,
-    unnamedAddr = Nothing,
-    type' = error "global alias type not defined",
-    addrSpace = AddrSpace 0,
-    aliasee = error "global alias aliasee not defined"
-  }
-
--- | helper for making 'Function's
-functionDefaults :: Global
-functionDefaults = 
-  Function {
-    linkage = L.External,
-    visibility = V.Default,
-    dllStorageClass = Nothing,
-    callingConvention = CC.C,
-    returnAttributes = [],
-    returnType = error "function return type not defined",
-    name = error "function name not defined",
-    parameters = ([], False),
-    functionAttributes = [],
-    section = Nothing,
-    comdat = Nothing,
-    alignment = 0,
-    garbageCollectorName = Nothing,
-    prefix = Nothing,
-    basicBlocks = [],
-    personalityFunction = Nothing
-  }
+// // helper for making 'Function's
+// functionDefaults: Global
+// functionDefaults =
+//   Function(
+//     linkage = L.External,
+//     visibility = V.Default,
+//     dllStorageClass = Nothing,
+//     callingConvention = CC.C,
+//     returnAttributes = [],
+//     returnType = error "function return type not defined",
+//     name = error "function name not defined",
+//     parameters = ([], False),
+//     functionAttributes = [],
+//     section = Nothing,
+//     comdat = Nothing,
+//     alignment = 0,
+//     garbageCollectorName = Nothing,
+//     prefix = Nothing,
+//     basicBlocks = [],
+//     personalityFunction = Nothing
+//   )
