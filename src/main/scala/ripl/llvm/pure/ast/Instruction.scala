41e0096fffab14b452acaf8e1dd93faf6e418726
ian <ianmccall93@gmail.com>
2018-06-04 14:48:17 -0700

    Port Instruction/Named

--- src/main/scala/ripl/llvm/pure/ast/Instruction.scala
+++ src/main/scala/ripl/llvm/pure/ast/Instruction.scala
@@ -437,6 +437,6 @@ case object Instruction {
 
 // Instances of instructions may be given a name, allowing their results to be referenced as 'Operand's.
 // Sometimes instructions - e.g. a call to a function returning void - don't need names.
-// sealed trait Named[a]
-// case class Name := a
-// case class Do a
+sealed trait Named[a]
+case class :=[A](name: Name, value: A) extends Named[A]
+case class Do[A](a: A) extends Named[A]
