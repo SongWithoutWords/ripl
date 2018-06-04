2a19a142a9b5ac2dfbfc6d3ecf193dd47c3f8858
ian <ianmccall93@gmail.com>
2018-06-04 01:22:05 -0700

    Port llvm/RMWOperation to scala

--- src/main/scala/ripl/llvm/pure/ast/RMWOperation.scala
+++ src/main/scala/ripl/llvm/pure/ast/RMWOperation.scala
@@ -1,22 +1,18 @@
--- | Operations for the 'LLVM.AST.Instruction.AtomicRMW' instruction
-module LLVM.AST.RMWOperation where
+// Operations for the 'LLVM.AST.Instruction.AtomicRMW' instruction
+package ripl.llvm.pure.ast
 
-import LLVM.Prelude
-
--- | <http://llvm.org/docs/LangRef.html#atomicrmw-instruction>
-data RMWOperation
-  = Xchg
-  | Add
-  | Sub
-  | And
-  | Nand
-  | Or
-  | Xor
-  | Max
-  | Min
-  | UMax
-  | UMin
-  deriving (Eq, Ord, Read, Show, Data, Typeable, Generic)
-
-
-
+// <http://llvm.org/docs/LangRef.html#atomicrmw-instruction>
+sealed trait RMWOperation
+case object RMWOperation {
+  case object Xchg extends RMWOperation
+  case object Add extends RMWOperation
+  case object Sub extends RMWOperation
+  case object And extends RMWOperation
+  case object Nand extends RMWOperation
+  case object Or extends RMWOperation
+  case object Xor extends RMWOperation
+  case object Max extends RMWOperation
+  case object Min extends RMWOperation
+  case object UMax extends RMWOperation
+  case object UMin extends RMWOperation
+}
