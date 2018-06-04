b41dde873a14b2c67dd045b7c2e0d8b820b869fc
ian <ianmccall93@gmail.com>
2018-06-04 14:31:47 -0700

    Port llvm/Typed to scala

--- src/main/scala/ripl/llvm/pure/ast/Typed.scala
+++ src/main/scala/ripl/llvm/pure/ast/Typed.scala
@@ -1,132 +1,176 @@
-// {-# LANGUAGE RecordWildCards #-}
+// Querying the type of LLVM expressions
+package ripl.llvm.pure.ast
 
--- | Querying the type of LLVM expressions
-module LLVM.AST.Typed (
-  Typed(..),
-  getElementType,
-  getElementPtrType,
-  extractValueType,
-) where
+import InstructionAliases._
+import OperandAliases._
+import TypeAliases._
 
-import LLVM.Prelude
+case object typeOf {
 
-import LLVM.AST
-import LLVM.AST.Global
-import LLVM.AST.Type
+  def apply(op: Operand): Type = op match {
+    case LocalReference(t, _) => t
+    case ConstantOperand(c)   => typeOf(c)
+    case _                    => MetadataType
+  }
 
-import qualified LLVM.AST.Constant as C
-import qualified LLVM.AST.Float as F
+  def apply(op: CallableOperand): Type = op match {
+    case Right(op) => typeOf(op)
+    case Left(_) =>
+      throw new Exception(
+        "typeOf inline assembler is not defined (Malformed AST)"
+      )
+  }
 
-class Typed a where
-    typeOf :: a -> Type
+  def apply(c: Constant): Type = c match {
+    case Integral(bits, _) => IntegerType(bits)
+    case Float(f)          => typeOf(f)
+    case Null(t)           => t
+    case Struct(_, isPacked, memberValues) =>
+      StructureType(isPacked, memberValues.map(typeOf(_)))
+    case Array(memberType, memberValues) =>
+      ArrayType(memberValues.size, memberType)
+    case Vector(memberValues) =>
+      VectorType(
+        memberValues.size,
+        memberValues match {
+          case Nil =>
+            throw new Exception(
+              "Vectors of size zero are not allowed. (Malformed AST)"
+            )
+          case (x :: _) => typeOf(x)
+        }
+      )
 
-instance Typed Operand where
-  typeOf (LocalReference t _) = t
-  typeOf (ConstantOperand c)  = typeOf c
-  typeOf _                    = MetadataType
+    case Undef(t)              => t
+    case c: BlockAddress       => ptr(i8)
+    case GlobalReference(t, _) => t
+    case c: Add                => typeOf(c.operand0)
+    case c: FAdd               => typeOf(c.operand0)
+    case c: FDiv               => typeOf(c.operand0)
+    case c: FRem               => typeOf(c.operand0)
+    case c: Sub                => typeOf(c.operand0)
+    case c: FSub               => typeOf(c.operand0)
+    case c: Mul                => typeOf(c.operand0)
+    case c: FMul               => typeOf(c.operand0)
+    case c: UDiv               => typeOf(c.operand0)
+    case c: SDiv               => typeOf(c.operand0)
+    case c: URem               => typeOf(c.operand0)
+    case c: SRem               => typeOf(c.operand0)
+    case c: Shl                => typeOf(c.operand0)
+    case c: LShr               => typeOf(c.operand0)
+    case c: AShr               => typeOf(c.operand0)
+    case c: And                => typeOf(c.operand0)
+    case c: Or                 => typeOf(c.operand0)
+    case c: Xor                => typeOf(c.operand0)
+    case c: GetElementPtr      => getElementPtrType(typeOf(c.address), c.indices)
+    case c: Trunc              => c.t
+    case c: ZExt               => c.t
+    case c: SExt               => c.t
+    case c: FPToUI             => c.t
+    case c: FPToSI             => c.t
+    case c: UIToFP             => c.t
+    case c: SIToFP             => c.t
+    case c: FPTrunc            => c.t
+    case c: FPExt              => c.t
+    case c: PtrToInt           => c.t
+    case c: IntToPtr           => c.t
+    case c: BitCast            => c.t
+    case c: ICmp =>
+      typeOf(c.operand0) match {
+        case VectorType(n, _) => VectorType(n, i1)
+        case _                => i1
+      }
 
-instance Typed CallableOperand where
-  typeOf (Right op) = typeOf op
-  typeOf (Left _) = error "typeOf inline assembler is not defined. (Malformed AST)"
+    case c: FCmp =>
+      typeOf(c.operand0) match {
+        case VectorType(n, _) => VectorType(n, i1)
+        case _                => i1
+      }
 
-instance Typed C.Constant where
-  typeOf (C.Int bits _)  = IntegerType bits
-  typeOf (C.Float t) = typeOf t
-  typeOf (C.Null t)      = t
-  typeOf (C.Struct {..}) = StructureType isPacked (map typeOf memberValues)
-  typeOf (C.Array {..})  = ArrayType (fromIntegral $ length memberValues) memberType
-  typeOf (C.Vector {..}) = VectorType (fromIntegral $ length memberValues) $
-                              case memberValues of
-                                  []    -> error "Vectors of size zero are not allowed. (Malformed AST)"
-                                  (x:_) -> typeOf x
-  typeOf (C.Undef t)     = t
-  typeOf (C.BlockAddress {..})   = ptr i8
-  typeOf (C.GlobalReference t _) = t
-  typeOf (C.Add {..})     = typeOf operand0
-  typeOf (C.FAdd {..})    = typeOf operand0
-  typeOf (C.FDiv {..})    = typeOf operand0
-  typeOf (C.FRem {..})    = typeOf operand0
-  typeOf (C.Sub {..})     = typeOf operand0
-  typeOf (C.FSub {..})    = typeOf operand0
-  typeOf (C.Mul {..})     = typeOf operand0
-  typeOf (C.FMul {..})    = typeOf operand0
-  typeOf (C.UDiv {..})    = typeOf operand0
-  typeOf (C.SDiv {..})    = typeOf operand0
-  typeOf (C.URem {..})    = typeOf operand0
-  typeOf (C.SRem {..})    = typeOf operand0
-  typeOf (C.Shl {..})     = typeOf operand0
-  typeOf (C.LShr {..})    = typeOf operand0
-  typeOf (C.AShr {..})    = typeOf operand0
-  typeOf (C.And {..})     = typeOf operand0
-  typeOf (C.Or  {..})     = typeOf operand0
-  typeOf (C.Xor {..})     = typeOf operand0
-  typeOf (C.GetElementPtr {..}) = getElementPtrType (typeOf address) indices
-  typeOf (C.Trunc {..})   = type'
-  typeOf (C.ZExt {..})    = type'
-  typeOf (C.SExt {..})    = type'
-  typeOf (C.FPToUI {..})  = type'
-  typeOf (C.FPToSI {..})  = type'
-  typeOf (C.UIToFP {..})  = type'
-  typeOf (C.SIToFP {..})  = type'
-  typeOf (C.FPTrunc {..}) = type'
-  typeOf (C.FPExt {..})   = type'
-  typeOf (C.PtrToInt {..}) = type'
-  typeOf (C.IntToPtr {..}) = type'
-  typeOf (C.BitCast {..})  = type'
-  typeOf (C.ICmp {..})    = case (typeOf operand0) of
-                              (VectorType n _) -> VectorType n i1
-                              _ -> i1
-  typeOf (C.FCmp {..})    = case (typeOf operand0) of
-                              (VectorType n _) -> VectorType n i1
-                              _ -> i1
-  typeOf (C.Select {..})  = typeOf trueValue
-  typeOf (C.ExtractElement {..})  = case typeOf vector of
-                                      (VectorType _ t) -> t
-                                      _ -> error "The first operand of an extractelement instruction is a value of vector type. (Malformed AST)"
-  typeOf (C.InsertElement {..})   = typeOf vector
-  typeOf (C.ShuffleVector {..})   = case (typeOf operand0, typeOf mask) of
-                                      (VectorType _ t, VectorType m _) -> VectorType m t
-                                      _ -> error "The first operand of an shufflevector instruction is a value of vector type. (Malformed AST)"
-  typeOf (C.ExtractValue {..})    = extractValueType indices' (typeOf aggregate)
-  typeOf (C.InsertValue {..})     = typeOf aggregate
-  typeOf (C.TokenNone)          = TokenType
-  typeOf (C.AddrSpaceCast {..}) = type'
+    case c: Select => typeOf(c.trueValue)
+    case c: ExtractElement =>
+      typeOf(c.vector) match {
+        case VectorType(_, t) => t
+        case _ =>
+          throw new Exception(
+            "The first operand of an extractelement instruction is a value of vector type. (Malformed AST)"
+          )
+      }
+    case c: InsertElement => typeOf(c.vector)
+    case c: ShuffleVector =>
+      (typeOf(c.operand0), typeOf(c.mask)) match {
+        case (VectorType(_, t), VectorType(m, _)) => VectorType(m, t)
+        case _ =>
+          throw new Exception(
+            "The first operand of an shufflevector instruction is a value of vector type. (Malformed AST)"
+          )
+      }
+    case c: ExtractValue  => extractValueType(c.indices, typeOf(c.aggregate))
+    case c: InsertValue   => typeOf(c.aggregate)
+    case TokenNone        => TokenType
+    case c: AddrSpaceCast => c.t
+  }
 
-getElementPtrType :: Type -> [C.Constant] -> Type
-getElementPtrType ty [] = ptr ty
-getElementPtrType (PointerType ty _) (_:is) = getElementPtrType ty is
-getElementPtrType (StructureType _ elTys) (C.Int 32 val:is) =
-  getElementPtrType (elTys !! fromIntegral val) is
-getElementPtrType (VectorType _ elTy) (_:is) = getElementPtrType elTy is
-getElementPtrType (ArrayType _ elTy) (_:is) = getElementPtrType elTy is
-getElementPtrType _ _ = error "Expecting aggregate type. (Malformed AST)"
+  def apply(f: SomeFloat): Type = f match {
+    // case Half(_)     => HalfFP
+    case Single(_) => FloatFP
+    case Double(_) => DoubleFP
+    // case F.Quadruple(_, _)   => FP128FP
+    // case F.X86_FP80(_, _)    => X86_FP80FP
+    // case F.PPC_FP128(_, _)   => PPC_FP128FP
+  }
 
-getElementType :: Type -> Type
-getElementType (PointerType t _) = t
-getElementType _ = error $ "Expecting pointer type. (Malformed AST)"
+  def apply(g: Global): Type = g match {
+    case g: GlobalVariable => g.t
+    case g: GlobalAlias    => g.t
+    case g: Function =>
+      val (params, isVarArg: Boolean) = g.parameters
+      FunctionType(g.returnType, params.map(typeOf(_)), isVarArg)
+  }
 
-extractValueType :: [Word32] -> Type -> Type
-extractValueType [] ty = ty
-extractValueType (i : is) (ArrayType numEls elTy)
-  | fromIntegral i < numEls = extractValueType is elTy
-  | fromIntegral i >= numEls = error "Expecting valid index into array type. (Malformed AST)"
-extractValueType (i : is) (StructureType _ elTys)
-  | fromIntegral i < length elTys = extractValueType is (elTys !! fromIntegral i)
-  | otherwise = error "Expecting valid index into structure type. (Malformed AST)"
-extractValueType _ _ = error "Expecting vector type. (Malformed AST)"
+  def apply(p: Parameter): Type = p.t
+}
 
-instance Typed F.SomeFloat where
-  typeOf (F.Half _)          = FloatingPointType HalfFP
-  typeOf (F.Single _)        = FloatingPointType FloatFP
-  typeOf (F.Double _)        = FloatingPointType DoubleFP
-  typeOf (F.Quadruple _ _)   = FloatingPointType FP128FP
-  typeOf (F.X86_FP80 _ _)    = FloatingPointType X86_FP80FP
-  typeOf (F.PPC_FP128 _ _)   = FloatingPointType PPC_FP128FP
+case object getElementPtrType {
+  def apply(t: Type, constants: List[Constant]): Type = (t, constants) match {
+    case (ty, Nil)                          => ptr(ty)
+    case (PointerType(ty, _), _ :: indices) => getElementPtrType(ty, indices)
+    case (StructureType(_, elementTypes), Integral(32, index) :: indices) =>
+      getElementPtrType(elementTypes(index), indices)
+    case (VectorType(_, elementType), _ :: indices) =>
+      getElementPtrType(elementType, indices)
+    case (ArrayType(_, elementType), _ :: indices) =>
+      getElementPtrType(elementType, indices)
+    case (_, _) =>
+      throw new Exception("Expecting aggregate type. (Malformed AST)")
+  }
+}
 
-instance Typed Global where
-  typeOf (GlobalVariable {..}) = type'
-  typeOf (GlobalAlias {..})    = type'
-  typeOf (Function {..})       = let (params, isVarArg) = parameters
-                                   in FunctionType returnType (map typeOf params) isVarArg
-instance Typed Parameter where
-  typeOf (Parameter t _ _) = t
+case object getElementType {
+  def apply(t: Type) = t match {
+    case PointerType(t, _) => t
+    case _                 => throw new Exception("Expecting pointer type. (Malformed AST)")
+  }
+}
+
+case object extractValueType {
+  def apply(indices: List[Int], t: Type): Type = (indices, t) match {
+    case (Nil, t) => t
+    case (i :: is, ArrayType(numElements, elementType)) =>
+      if (i < numElements) {
+        extractValueType(is, elementType)
+      } else {
+        throw new Exception(
+          "Expecting valid index into array type. (Malformed AST)"
+        )
+      }
+    case (i :: is, StructureType(_, elementTypes)) =>
+      if (i < elementTypes.size) {
+        extractValueType(is, elementTypes(i))
+      } else {
+        throw new Exception(
+          "Expecting valid index into structure type. (Malformed AST)"
+        )
+      }
+  }
+}
