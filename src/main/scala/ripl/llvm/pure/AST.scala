b70cb99d0eb0ed9c4a31b797a736cfcc9386aeef
ian <ianmccall93@gmail.com>
2018-06-04 16:53:11 -0700

    Port llvm/AST to scala

--- src/main/scala/ripl/llvm/pure/AST.scala
+++ src/main/scala/ripl/llvm/pure/AST.scala
@@ -1,64 +1,71 @@
-{-# LANGUAGE OverloadedStrings #-}
--- | This module and descendants define AST data types to represent LLVM code.
--- Note that these types are designed for fidelity rather than convenience - if the truth
--- of what LLVM supports is less than pretty, so be it.
-module LLVM.AST (
-  Module(..), defaultModule,
-  Definition(..),
-  Global(GlobalVariable, GlobalAlias, Function),
-        globalVariableDefaults,
-        globalAliasDefaults,
-        functionDefaults,
-  UnnamedAddr(..),
-  Parameter(..),
-  BasicBlock(..),
-  module LLVM.AST.Instruction,
-  module LLVM.AST.Name,
-  module LLVM.AST.Operand,
-  module LLVM.AST.Type
-  ) where
+// This module and descendants define AST data types to represent LLVM code.
+// Note that these types are designed for fidelity rather than convenience - if the truth
+// of what LLVM supports is less than pretty, so be it.
 
-import LLVM.Prelude
+package ripl.llvm.pure.ast
 
-import LLVM.AST.Name
-import LLVM.AST.Type (Type(..), FloatingPointType(..))
-import LLVM.AST.Global
-import LLVM.AST.Operand
-import LLVM.AST.Instruction
-import LLVM.AST.DataLayout
-import qualified LLVM.AST.Attribute as A
-import qualified LLVM.AST.COMDAT as COMDAT
+// module LLVM.AST (
+//   Module(..), defaultModule,
+//   Definition(..),
+//   Global(GlobalVariable, GlobalAlias, Function),
+//         globalVariableDefaults,
+//         globalAliasDefaults,
+//         functionDefaults,
+//   UnnamedAddr(..),
+//   Parameter(..),
+//   BasicBlock(..),
+//   module LLVM.AST.Instruction,
+//   module LLVM.AST.Name,
+//   module LLVM.AST.Operand,
+//   module LLVM.AST.Type
+//   ) where
 
--- | Any thing which can be at the top level of a 'Module'
-data Definition
-  = GlobalDefinition Global
-  | TypeDefinition Name (Maybe Type)
-  | MetadataNodeDefinition MetadataNodeID [Maybe Metadata]
-  | NamedMetadataDefinition ShortByteString [MetadataNodeID]
-  | ModuleInlineAssembly ByteString
-  | FunctionAttributes A.GroupID [A.FunctionAttribute]
-  | COMDAT ShortByteString COMDAT.SelectionKind
-    deriving (Eq, Read, Show, Typeable, Data, Generic)
+// import LLVM.Prelude
 
--- | <http://llvm.org/docs/LangRef.html#module-structure>
-data Module =
-  Module {
-    moduleName :: ShortByteString,
-    moduleSourceFileName :: ShortByteString,
-    -- | a 'DataLayout', if specified, must match that of the eventual code generator
-    moduleDataLayout :: Maybe DataLayout,
-    moduleTargetTriple :: Maybe ShortByteString,
-    moduleDefinitions :: [Definition]
-  }
-  deriving (Eq, Read, Show, Typeable, Data, Generic)
+// import LLVM.AST.Name
+// import LLVM.AST.Type (Type(..), FloatingPointType(..))
+// import LLVM.AST.Global
+// import LLVM.AST.Operand
+// import LLVM.AST.Instruction
+// import LLVM.AST.DataLayout
+// import qualified LLVM.AST.Attribute as A
+// import qualified LLVM.AST.COMDAT as COMDAT
 
--- | helper for making 'Module's
-defaultModule :: Module
-defaultModule =
-  Module {
-    moduleName = "<string>",
-    moduleSourceFileName = "<string>",
-    moduleDataLayout = Nothing,
-    moduleTargetTriple = Nothing,
-    moduleDefinitions = []
-  }
+// Any thing which can be at the top level of a 'Module'
+sealed trait Definition
+case class GlobalDefinition(g: Global) extends Definition
+case class TypeDefinition(name: Name, t: Option[Type]) extends Definition
+case class MetadataNodeDefinition(
+    nodeID: MetadataNodeID,
+    metaData: List[Option[Metadata]]
+) extends Definition
+case class NamedMetadataDefinition(name: String, metaData: List[MetadataNodeID])
+    extends Definition
+case class ModuleInlineAssembly(asm: String) extends Definition
+case class FunctionAttributes(
+    groupId: GroupID,
+    functionAttributs: List[FunctionAttribute]
+) extends Definition
+case class COMDAT(string: String, selectionKind: SelectionKind)
+    extends Definition
+
+// <http://llvm.org/docs/LangRef.html#module-structure>
+case class Module(
+    moduleName: String,
+    moduleSourceFileName: String,
+    // a 'DataLayout', if specified, must match that of the eventual code generator
+    moduleDataLayout: Option[DataLayout],
+    moduleTargetTriple: Option[String],
+    moduleDefinitions: List[Definition]
+)
+
+// helper for making 'Module's
+// defaultModule: Module
+// defaultModule =
+//   Module {
+//     moduleName = "<string>",
+//     moduleSourceFileName = "<string>",
+//     moduleDataLayout = Nothing,
+//     moduleTargetTriple = Nothing,
+//     moduleDefinitions = []
+//   }
