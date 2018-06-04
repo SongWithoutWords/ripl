b6146271f9c38dd15f3847c356a0fbfb39b7ac4a
ian <ianmccall93@gmail.com>
2018-06-04 00:49:32 -0700

    Port llvm/Operand to Scala

--- src/main/scala/ripl/llvm/pure/ast/Operand.scala
+++ src/main/scala/ripl/llvm/pure/ast/Operand.scala
@@ -1,40 +1,34 @@
--- | A type to represent operands to LLVM 'LLVM.AST.Instruction.Instruction's
-module LLVM.AST.Operand where
+// A type to represent operands to LLVM 'LLVM.AST.Instruction.Instruction's
 
-import LLVM.Prelude
+package ripl.llvm.pure.ast
 
-import LLVM.AST.Name
-import LLVM.AST.Constant
-import LLVM.AST.InlineAssembly
-import LLVM.AST.Type
+// A 'MetadataNodeID' is a number for identifying a metadata node.
+// Note this is different from "named metadata", which are represented with
+// 'LLVM.AST.NamedMetadataDefinition'.
+// newtype MetadataNodeID = MetadataNodeID Word
+case class MetadataNodeID(id: Int)
 
--- | A 'MetadataNodeID' is a number for identifying a metadata node.
--- Note this is different from "named metadata", which are represented with
--- 'LLVM.AST.NamedMetadataDefinition'.
-newtype MetadataNodeID = MetadataNodeID Word
-  deriving (Eq, Ord, Read, Show, Typeable, Data, Generic)
+// <http://llvm.org/docs/LangRef.html#metadata>
+sealed trait MetadataNode
+case class MetadataNodeData(data: List[Option[Metadata]]) extends MetadataNode
+case class MetadataNodeReference(data: MetadataNodeID) extends MetadataNode
 
--- | <http://llvm.org/docs/LangRef.html#metadata>
-data MetadataNode 
-  = MetadataNode [Maybe Metadata]
-  | MetadataNodeReference MetadataNodeID
-  deriving (Eq, Ord, Read, Show, Typeable, Data, Generic)
+// <http://llvm.org/docs/LangRef.html#metadata>
+sealed trait Metadata
+// ^ <http://llvm.org/docs/doxygen/html/classllvm_1_1MDNode.html>
+case class MDString(string: String) extends Metadata
+// ^ <http://llvm.org/docs/doxygen/html/classllvm_1_1MDNode.html>
+case class MDNode(node: MetadataNode) extends Metadata
+// ^ <http://llvm.org/docs/doxygen/html/classllvm_1_1ValueAsMetadata.html>
+case class MDValue(operand: Operand) extends Metadata
 
--- | <http://llvm.org/docs/LangRef.html#metadata>
-data Metadata
-  = MDString ShortByteString -- ^ <http://llvm.org/docs/doxygen/html/classllvm_1_1MDNode.html>
-  | MDNode MetadataNode -- ^ <http://llvm.org/docs/doxygen/html/classllvm_1_1MDNode.html>
-  | MDValue Operand -- ^ <http://llvm.org/docs/doxygen/html/classllvm_1_1ValueAsMetadata.html>
-  deriving (Eq, Ord, Read, Show, Typeable, Data, Generic)
+// An 'Operand' is roughly that which is an argument to an 'LLVM.AST.Instruction.Instruction'
+sealed trait Operand
+// %foo
+case class LocalReference(t: Type, name: Name) extends Operand
+// 'Constant's include 'LLVM.AST.Constant.GlobalReference', for \@foo
+case class ConstantOperand(constant: Constant) extends Operand
+case class MetadataOperand(metadata: Metadata) extends Operand
 
--- | An 'Operand' is roughly that which is an argument to an 'LLVM.AST.Instruction.Instruction'
-data Operand 
-  -- | %foo
-  = LocalReference Type Name
-  -- | 'Constant's include 'LLVM.AST.Constant.GlobalReference', for \@foo
-  | ConstantOperand Constant
-  | MetadataOperand Metadata
-  deriving (Eq, Ord, Read, Show, Typeable, Data, Generic)
-
--- | The 'LLVM.AST.Instruction.Call' instruction is special: the callee can be inline assembly
-type CallableOperand  = Either InlineAssembly Operand
+// The 'LLVM.AST.Instruction.Call' instruction is special: the callee can be inline assembly
+// type CallableOperand = Either[InlineAssembly, Operand]
