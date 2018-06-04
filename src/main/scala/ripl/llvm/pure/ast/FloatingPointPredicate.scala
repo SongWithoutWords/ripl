32d8936ae24f5aaba38e085e26f404e55806ccba
ian <ianmccall93@gmail.com>
2018-06-03 17:30:27 -0700

    Ported ast/FloatingPointPredicate from hs to scala

---
+++ src/main/scala/ripl/llvm/pure/ast/FloatingPointPredicate.scala
@@ -0,0 +1,25 @@
+// Predicates for the 'LLVM.AST.Instruction.FCmp' instruction
+package ripl.llvm.pure.ast
+
+// <http://llvm.org/docs/LangRef.html#fcmp-instruction>
+
+sealed trait FloatingPointPredicate
+
+case object FloatingPointPredicate {
+  case object False extends FloatingPointPredicate
+  case object OEQ extends FloatingPointPredicate
+  case object OGT extends FloatingPointPredicate
+  case object OGE extends FloatingPointPredicate
+  case object OLT extends FloatingPointPredicate
+  case object OLE extends FloatingPointPredicate
+  case object ONE extends FloatingPointPredicate
+  case object ORD extends FloatingPointPredicate
+  case object UNO extends FloatingPointPredicate
+  case object UEQ extends FloatingPointPredicate
+  case object UGT extends FloatingPointPredicate
+  case object UGE extends FloatingPointPredicate
+  case object ULT extends FloatingPointPredicate
+  case object ULE extends FloatingPointPredicate
+  case object UNE extends FloatingPointPredicate
+  case object True extends FloatingPointPredicate
+}
