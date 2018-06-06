340ff37d93ac81c1f47e6429420e7a3e1ba26576
ian <ianmccall93@gmail.com>
2018-06-05 20:26:26 -0700

    Disambiguate data of llvm floating point constants

--- src/main/scala/ripl/llvm/pure/ast/Float.scala
+++ src/main/scala/ripl/llvm/pure/ast/Float.scala
@@ -5,8 +5,8 @@ package ripl.llvm.pure.ast
 
 sealed trait SomeFloat
 //   case class Half(value: Word16)
-case class Single(value: Float) extends SomeFloat
-case class Double(value: Float) extends SomeFloat
+case class Single(value: scala.Float) extends SomeFloat
+case class Double(value: scala.Double) extends SomeFloat
 //   case class Quadruple(high: Word64, low: Word64)
 //   case class X86_FP80(high: Word16, low: Word64)
 //   case class PPC_FP128(high: Word64, low: Word64)
