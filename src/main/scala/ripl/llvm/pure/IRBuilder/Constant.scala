d870fe70ddbbd960c59d0408c90cd81d1f6c8360
ian <ianmccall93@gmail.com>
2018-06-05 20:23:38 -0700

    Integrated llvm.irBuilder.Constant with cats.Applicative

--- src/main/scala/ripl/llvm/pure/IRBuilder/Constant.scala
+++ src/main/scala/ripl/llvm/pure/IRBuilder/Constant.scala
@@ -2,33 +2,35 @@ package ripl.llvm.pure.IRBuilder
 
 import ripl.llvm.pure.ast._
 
+import cats._
+import cats.implicits._
+
 case object Constant {
-  // def int64[F[_]](i: Long): F[Operand] =
-  //   pure(ConstantOperand(Integral(64, i)))
+  def int64[F[_]: Applicative](i: Long): F[Operand] =
+    Applicative[F].pure(ConstantOperand(Integral(64, BigInt(i))))
 
-  // def int32[F[_]](i: Long): F[Operand] =
-  //   pure(ConstantOperand(Integral(32, i)))
+  def int32[F[_]: Applicative](i: Long): F[Operand] =
+    Applicative[F].pure(ConstantOperand(Integral(32, i)))
 
-  // def bit[F[_]](b: Boolean): F[Operand] =
-  //   pure(ConstantOperand(Integral(1, if (b) 1 else 0)))
+  def bit[F[_]: Applicative](b: Boolean): F[Operand] =
+    Applicative[F].pure(ConstantOperand(Integral(1, if (b) 1 else 0)))
 
-  // def double[F[_]](f: scala.Double): F[Operand] =
-  //   pure(ConstantOperand(Float(Double(f))))
+  def double[F[_]: Applicative](f: scala.Double): F[Operand] =
+    Applicative[F].pure(ConstantOperand(Float(Double(f))))
 
-  // def single[F[_]](f: scala.Float): F[Operand] =
-  //   pure(ConstantOperand(Float(Single(f))))
+  def single[F[_]: Applicative](f: scala.Float): F[Operand] =
+    Applicative[F].pure(ConstantOperand(Float(Single(f))))
 
-  // def half[f <: Applicative[_]](f: Float): f[Operand] =
-  //   pure(ConstantOperand(Single(1, i)))
+  // def half[F[_]: Applicative](f: Short): F[Operand] =
+  //   Applicative[F].pure(ConstantOperand(Single(1, i)))
 
-  // def struct[F[_] <: Applicative](
-  //     name: Option[Name],
-  //     packed: Boolean,
-  //     members: List[Constant],
-  //     pure: Pure[A]
-  // ): F[Operand] =
-  //   pure(ConstantOperand(Struct(name, packed, members)))
+  def struct[F[_]: Applicative](
+      name: Option[Name],
+      packed: Boolean,
+      members: List[Constant]
+  ): F[Operand] =
+    Applicative[F].pure(ConstantOperand(Struct(name, packed, members)))
 
-  // def struct[F[_]](members: List[Constant]): F[Operand] =
-  //   pure(ConstantOperand(Array(typeOf(members.head), members)))
+  def struct[F[_]: Applicative](members: List[Constant]): F[Operand] =
+    Applicative[F].pure(ConstantOperand(Array(typeOf(members.head), members)))
 }
