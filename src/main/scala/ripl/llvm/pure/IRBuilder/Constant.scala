0897c1ccaeca4f710cabdf632ee245097afef7cc
ian <ianmccall93@gmail.com>
2018-06-14 17:39:50 -0700

    Wrap constants in Constant object to avoid potential name clashes

--- src/main/scala/ripl/llvm/pure/IRBuilder/Constant.scala
+++ src/main/scala/ripl/llvm/pure/IRBuilder/Constant.scala
@@ -6,6 +6,8 @@ import cats._
 import cats.implicits._
 
 case object Constant {
+  import ripl.llvm.pure.ast.Constant._
+
   def int64[F[_]: Applicative](i: Long): F[Operand] =
     Applicative[F].pure(ConstantOperand(Integral(64, BigInt(i))))
 
--- src/main/scala/ripl/llvm/pure/ast/Typed.scala
+++ src/main/scala/ripl/llvm/pure/ast/Typed.scala
@@ -19,94 +19,97 @@ case object typeOf {
       )
   }
 
-  def apply(c: Constant): Type = c match {
-    case Integral(bits, _) => IntegerType(bits)
-    case Float(f)          => typeOf(f)
-    case Null(t)           => t
-    case Struct(_, isPacked, memberValues) =>
-      StructureType(isPacked, memberValues.map(typeOf(_)))
-    case Array(memberType, memberValues) =>
-      ArrayType(memberValues.size, memberType)
-    case Vector(memberValues) =>
-      VectorType(
-        memberValues.size,
-        memberValues match {
-          case Nil =>
-            throw new Exception(
-              "Vectors of size zero are not allowed. (Malformed AST)"
-            )
-          case (x :: _) => typeOf(x)
+  def apply(c: Constant): Type = {
+    import Constant._
+    c match {
+      case Integral(bits, _) => IntegerType(bits)
+      case Float(f)          => typeOf(f)
+      case Null(t)           => t
+      case Struct(_, isPacked, memberValues) =>
+        StructureType(isPacked, memberValues.map(typeOf(_)))
+      case Array(memberType, memberValues) =>
+        ArrayType(memberValues.size, memberType)
+      case Vector(memberValues) =>
+        VectorType(
+          memberValues.size,
+          memberValues match {
+            case Nil =>
+              throw new Exception(
+                "Vectors of size zero are not allowed. (Malformed AST)"
+              )
+            case (x :: _) => typeOf(x)
+          }
+        )
+
+      case Undef(t)              => t
+      case c: BlockAddress       => ptr(i8)
+      case GlobalReference(t, _) => t
+      case c: Add                => typeOf(c.operand0)
+      case c: FAdd               => typeOf(c.operand0)
+      case c: FDiv               => typeOf(c.operand0)
+      case c: FRem               => typeOf(c.operand0)
+      case c: Sub                => typeOf(c.operand0)
+      case c: FSub               => typeOf(c.operand0)
+      case c: Mul                => typeOf(c.operand0)
+      case c: FMul               => typeOf(c.operand0)
+      case c: UDiv               => typeOf(c.operand0)
+      case c: SDiv               => typeOf(c.operand0)
+      case c: URem               => typeOf(c.operand0)
+      case c: SRem               => typeOf(c.operand0)
+      case c: Shl                => typeOf(c.operand0)
+      case c: LShr               => typeOf(c.operand0)
+      case c: AShr               => typeOf(c.operand0)
+      case c: And                => typeOf(c.operand0)
+      case c: Or                 => typeOf(c.operand0)
+      case c: Xor                => typeOf(c.operand0)
+      case c: GetElementPtr      => getElementPtrType(typeOf(c.address), c.indices)
+      case c: Trunc              => c.t
+      case c: ZExt               => c.t
+      case c: SExt               => c.t
+      case c: FPToUI             => c.t
+      case c: FPToSI             => c.t
+      case c: UIToFP             => c.t
+      case c: SIToFP             => c.t
+      case c: FPTrunc            => c.t
+      case c: FPExt              => c.t
+      case c: PtrToInt           => c.t
+      case c: IntToPtr           => c.t
+      case c: BitCast            => c.t
+      case c: ICmp =>
+        typeOf(c.operand0) match {
+          case VectorType(n, _) => VectorType(n, i1)
+          case _                => i1
         }
-      )
 
-    case Undef(t)              => t
-    case c: BlockAddress       => ptr(i8)
-    case GlobalReference(t, _) => t
-    case c: Add                => typeOf(c.operand0)
-    case c: FAdd               => typeOf(c.operand0)
-    case c: FDiv               => typeOf(c.operand0)
-    case c: FRem               => typeOf(c.operand0)
-    case c: Sub                => typeOf(c.operand0)
-    case c: FSub               => typeOf(c.operand0)
-    case c: Mul                => typeOf(c.operand0)
-    case c: FMul               => typeOf(c.operand0)
-    case c: UDiv               => typeOf(c.operand0)
-    case c: SDiv               => typeOf(c.operand0)
-    case c: URem               => typeOf(c.operand0)
-    case c: SRem               => typeOf(c.operand0)
-    case c: Shl                => typeOf(c.operand0)
-    case c: LShr               => typeOf(c.operand0)
-    case c: AShr               => typeOf(c.operand0)
-    case c: And                => typeOf(c.operand0)
-    case c: Or                 => typeOf(c.operand0)
-    case c: Xor                => typeOf(c.operand0)
-    case c: GetElementPtr      => getElementPtrType(typeOf(c.address), c.indices)
-    case c: Trunc              => c.t
-    case c: ZExt               => c.t
-    case c: SExt               => c.t
-    case c: FPToUI             => c.t
-    case c: FPToSI             => c.t
-    case c: UIToFP             => c.t
-    case c: SIToFP             => c.t
-    case c: FPTrunc            => c.t
-    case c: FPExt              => c.t
-    case c: PtrToInt           => c.t
-    case c: IntToPtr           => c.t
-    case c: BitCast            => c.t
-    case c: ICmp =>
-      typeOf(c.operand0) match {
-        case VectorType(n, _) => VectorType(n, i1)
-        case _                => i1
-      }
+      case c: FCmp =>
+        typeOf(c.operand0) match {
+          case VectorType(n, _) => VectorType(n, i1)
+          case _                => i1
+        }
 
-    case c: FCmp =>
-      typeOf(c.operand0) match {
-        case VectorType(n, _) => VectorType(n, i1)
-        case _                => i1
-      }
-
-    case c: Select => typeOf(c.trueValue)
-    case c: ExtractElement =>
-      typeOf(c.vector) match {
-        case VectorType(_, t) => t
-        case _ =>
-          throw new Exception(
-            "The first operand of an extractelement instruction is a value of vector type. (Malformed AST)"
-          )
-      }
-    case c: InsertElement => typeOf(c.vector)
-    case c: ShuffleVector =>
-      (typeOf(c.operand0), typeOf(c.mask)) match {
-        case (VectorType(_, t), VectorType(m, _)) => VectorType(m, t)
-        case _ =>
-          throw new Exception(
-            "The first operand of an shufflevector instruction is a value of vector type. (Malformed AST)"
-          )
-      }
-    case c: ExtractValue  => extractValueType(c.indices, typeOf(c.aggregate))
-    case c: InsertValue   => typeOf(c.aggregate)
-    case TokenNone        => TokenType
-    case c: AddrSpaceCast => c.t
+      case c: Select => typeOf(c.trueValue)
+      case c: ExtractElement =>
+        typeOf(c.vector) match {
+          case VectorType(_, t) => t
+          case _ =>
+            throw new Exception(
+              "The first operand of an extractelement instruction is a value of vector type. (Malformed AST)"
+            )
+        }
+      case c: InsertElement => typeOf(c.vector)
+      case c: ShuffleVector =>
+        (typeOf(c.operand0), typeOf(c.mask)) match {
+          case (VectorType(_, t), VectorType(m, _)) => VectorType(m, t)
+          case _ =>
+            throw new Exception(
+              "The first operand of an shufflevector instruction is a value of vector type. (Malformed AST)"
+            )
+        }
+      case c: ExtractValue  => extractValueType(c.indices, typeOf(c.aggregate))
+      case c: InsertValue   => typeOf(c.aggregate)
+      case TokenNone        => TokenType
+      case c: AddrSpaceCast => c.t
+    }
   }
 
   def apply(f: SomeFloat): Type = f match {
@@ -137,7 +140,10 @@ case object getElementPtrType {
   def apply(t: Type, constants: List[Constant]): Type = (t, constants) match {
     case (ty, Nil)                          => ptr(ty)
     case (PointerType(ty, _), _ :: indices) => getElementPtrType(ty, indices)
-    case (StructureType(_, elementTypes), Integral(32, index) :: indices) =>
+    case (
+        StructureType(_, elementTypes),
+        Constant.Integral(32, index) :: indices
+        ) =>
       getElementPtrType(elementTypes(index.toInt), indices)
     case (VectorType(_, elementType), _ :: indices) =>
       getElementPtrType(elementType, indices)
