6fe7014f7c4d6ef6c01ae53000fdd2845dc10d06
ian <ianmccall93@gmail.com>
2018-06-18 21:10:05 -0700

    File declaring tokens

---
+++ src/main/scala/ripl/parse/recursive/Token.scala
@@ -0,0 +1,21 @@
+package ripl.parse.recursive
+
+sealed trait Token
+case object Token {
+  case object Newline          extends Token
+  case object LParen           extends Token
+  case object RParen           extends Token
+
+  case class Symbol(s: String) extends Token
+  case class Bool(v: Boolean)  extends Token
+  case class Float(v: Float)   extends Token
+  case class Int(v: Long)      extends Token
+  case class String(v: String) extends Token
+
+  sealed trait PrefixOperator extends Token
+  case object Apostrophe extends PrefixOperator
+  case object Tilda extends PrefixOperator
+  case object Circumflex extends PrefixOperator
+}
+
+
