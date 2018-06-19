3c35b699e1fed380065cd081337a317cac285c68
ian <ianmccall93@gmail.com>
2018-06-18 21:25:13 -0700

    Slight reorganization of tokens

--- src/main/scala/ripl/parse/recursive/Token.scala
+++ src/main/scala/ripl/parse/recursive/Token.scala
@@ -6,16 +6,18 @@ case object Token {
   case object LParen           extends Token
   case object RParen           extends Token
 
-  case class Symbol(s: String) extends Token
-  case class Bool(v: Boolean)  extends Token
-  case class Float(v: Float)   extends Token
-  case class Int(v: Long)      extends Token
-  case class String(v: String) extends Token
-
   sealed trait PrefixOperator extends Token
   case object Apostrophe extends PrefixOperator
   case object Tilda extends PrefixOperator
   case object Circumflex extends PrefixOperator
+
+  case class Symbol(s: String) extends Token
+
+  // value atoms
+  case class Bool(v: Boolean)  extends Token
+  case class Float(v: Float)   extends Token
+  case class Int(v: Long)      extends Token
+  case class String(v: String) extends Token
 }
 
 
