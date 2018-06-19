0a5b0f50dcfaded36e350504a59aa922d2357772
ian <ianmccall93@gmail.com>
2018-06-19 00:46:27 -0700

    Initial lexer tests

---
+++ src/test/scala/ripl/parse/recursive/Lex.scala
@@ -0,0 +1,32 @@
+package ripl.parse.recursive
+
+import org.scalatest._
+
+import ripl.parse.recursive._
+
+import ripl.reduce.CustomMatchers.matchAst
+
+class TestLexer extends FreeSpec with Matchers {
+
+  def test(name: String, input: String)(out: Token*): Unit = name in {
+    Lex(input) shouldBe out
+  }
+  def test(input: String)(out: Token*): Unit =
+    test(input, input)(out: _*)
+
+  import Token._
+
+  "single tokens" - {
+
+    test("\n")(Newline)
+
+    test("(")(LParen)
+    test(")")(RParen)
+
+    test("'")(Apostrophe)
+    test("^")(Circumflex)
+    test("~")(Tilda)
+
+  }
+
+}
