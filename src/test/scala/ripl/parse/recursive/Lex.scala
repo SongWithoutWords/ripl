53dc2f58094b806339cd4f0832f41a8cf504fd81
ian <ianmccall93@gmail.com>
2018-06-21 00:28:41 -0700

    Lex test for lisp-case

--- src/test/scala/ripl/parse/recursive/Lex.scala
+++ src/test/scala/ripl/parse/recursive/Lex.scala
@@ -40,6 +40,7 @@ class TestLexer extends FreeSpec with Matchers {
         testName("camelCase")
         testName("StudlyCase")
         testName("snake_case")
+        testName("kebab-case")
         testName("SCREAMING_SNAKE_CASE")
         testName("_surrounded_with_under_scores_")
       }
