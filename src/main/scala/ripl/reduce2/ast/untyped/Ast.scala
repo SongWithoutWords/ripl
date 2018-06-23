ee2ec8228f4a3572d78dffae27beb3d7fa2c1cc5
ian <ianmccall93@gmail.com>
2018-06-23 14:12:59 -0700

    Initial untyped ast

--- src/main/scala/ripl/reduce2/ast/untyped/Ast.scala
+++ src/main/scala/ripl/reduce2/ast/untyped/Ast.scala
@@ -1,1 +1,42 @@
 package ripl.reduce2.ast.untyped
+
+import scala.collection.mutable.ArrayBuffer
+
+import ripl.util.MultiMap
+import ripl.ast.parse._
+import ripl.ast.common._
+
+// Unique identifier of a definition within the program
+case class ID(value: Int)
+
+case class Definitions(contents: ArrayBuffer[Exp]) {
+  def apply(id: ID): Exp = contents(id.value)
+
+  def appendNew(a: Exp): ID = {
+    val count = contents.length
+    contents.append(a)
+    ID(count)
+  }
+}
+
+case class Scopes(contents: ArrayBuffer[Scope]) {
+  def apply(id: ID): Scope = contents(id.value)
+}
+
+case object Scope {
+  type Contents = MultiMap[Name, Either[ID, Scope]]
+}
+
+sealed trait Scope {
+  def lookup(n: Name): List[Either[ID, Scope]]
+}
+
+case class Global(contents: Scope.Contents) extends Scope {
+  def lookup(n: Name) = contents(n)
+}
+
+case class Local(contents: Scope.Contents, parent: Scope) extends Scope {
+  def lookup(n: Name) = contents(n) ++ parent.lookup(n)
+}
+
+case class Ast(definitions: Definitions, scopes: Scopes)
