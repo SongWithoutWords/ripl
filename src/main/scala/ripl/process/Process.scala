2bdd820603b7be9da1a430281f084ae12d31b74f
ian <ianmccall93@gmail.com>
2018-06-24 13:36:31 -0700

    Process module

--- src/main/scala/ripl/process/Process.scala
+++ src/main/scala/ripl/process/Process.scala
@@ -1,17 +1,47 @@
 package ripl.process
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.charset.StandardCharsets
+
+import sys.process._
+
 import reduce._
 
-import ripl.llvm.pretty.prettyPrint
+import ripl.ast._
+import ripl.parse.Lex
+import ripl.parse.Parse
+import ripl.reduce.Reduce
+import ripl.codegen.CodeGen
+import ripl.llvm.pretty.PrettyPrint
 
-case object GenerateLlvmIr {
-  def apply(s: String): Either[String, Set[Error]] = ???
+case object LexParse {
+  def apply(riplSrc: String) = Parse(Lex(riplSrc))
 }
 
-case object Compile {
-  def apply(s: String): Option[Error] = ???
+case object LexParseReduce {
+  def apply(riplSrc: String) = Reduce(LexParse(riplSrc))
+}
+
+case object GenerateLlvmAst {
+  def apply(riplSrc: String) = {
+    val (ast, errors) = LexParseReduce(riplSrc)
+    if (errors.isEmpty) {
+      Right(CodeGen(ast))
+    } else Left(errors)
+  }
+}
+
+case object GenerateLlvmIr {
+  def apply(riplSrc: String) = GenerateLlvmAst(riplSrc).map(PrettyPrint(_))
 }
 
 case object Run {
-
+  def apply(path: Path, riplSrc: String) = GenerateLlvmIr(riplSrc) match {
+    case Left(errors) => Left(errors)
+    case Right(llvmIrSrc) =>
+      Files.write(path, llvmIrSrc.getBytes(StandardCharsets.UTF_8))
+      val process = Process(Seq("lli", path.toString))
+      process.!
+  }
 }
