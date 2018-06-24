45397fc51b9141fb6a3cdb05f50847d91d1dd419
ian <ianmccall93@gmail.com>
2018-06-24 15:12:14 -0700

    genModule, genUnit, genParam and signature for genType

--- src/main/scala/ripl/codegen/CodeGen.scala
+++ src/main/scala/ripl/codegen/CodeGen.scala
@@ -1,8 +1,31 @@
 package ripl.codegen
 
-import ripl.ast.{typed => t}
+import ripl.ast.typed._
 import ripl.llvm.{ast => l}
 
 case object CodeGen {
-  def apply(a: t.Ast): l.Module = ???
+  def apply(ast: Ast): l.Module = genModule(ast)
+
+  def genModule(ast: Ast): l.Module = {
+    val definitions = ast.toList.map { case (name, node) => genUnit(name, node) }
+    l.Module("ripl", "", None, None, definitions)
+  }
+
+  def genUnit(name: String, node: Node): l.Definition = l.GlobalDefinition(
+    node match {
+      case Fun(params, rType, exp) =>
+        l.Function(
+          callingConvention = l.CallingConvention.Fast,
+          returnType = genType(rType),
+          name = l.Name(name),
+          parameters = l.Parameters(params.map(genParam))
+        )
+    }
+  )
+
+  def genType(t: Type): l.Type = t match {
+    case _ => ???
+  }
+
+  def genParam(p: Param): l.Parameter = l.Parameter(genType(p.t), l.Name(p.n))
 }
