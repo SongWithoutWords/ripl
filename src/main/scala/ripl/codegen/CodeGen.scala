1b8c25e50bd352c6e9e3b33d81ca5c02d8173459
ian <ianmccall93@gmail.com>
2018-06-25 09:55:34 -0700

    CodeGen genFun and genExp

--- src/main/scala/ripl/codegen/CodeGen.scala
+++ src/main/scala/ripl/codegen/CodeGen.scala
@@ -1,13 +1,26 @@
 package ripl.codegen
 
+import cats._
+import cats.data.State
+import cats.syntax.MonadOps
+import cats.instances._
+import cats.implicits._
+
 import ripl.ast.typed._
-import ripl.llvm.{ast => l}
+import ripl.ast.common._
+import ripl.ast.common.TypeAtom._
+
+import ripl.llvm.{ast => l, irBuilder => m}
+import ripl.llvm.irBuilder.{IRBuilderInstruction => i}
+import ripl.llvm.irBuilder.{Constant => c}
 
 case object CodeGen {
   def apply(ast: Ast): l.Module = genModule(ast)
 
   def genModule(ast: Ast): l.Module = {
-    val definitions = ast.toList.map { case (name, node) => genUnit(name, node) }
+    val definitions = ast.toList.map {
+      case (name, node) => genUnit(name, node)
+    }
     l.Module("ripl", "", None, None, definitions)
   }
 
@@ -16,16 +29,48 @@ case object CodeGen {
       case Fun(params, rType, exp) =>
         l.Function(
           callingConvention = l.CallingConvention.Fast,
-          returnType = genType(rType),
           name = l.Name(name),
-          parameters = l.Parameters(params.map(genParam))
+          parameters = l.Parameters(params.map(genParam)),
+          returnType = genType(rType),
+          basicBlocks = genFun(params, exp)
         )
     }
   )
 
-  def genType(t: Type): l.Type = t match {
-    case _ => ???
+  def genParam(p: Param): l.Parameter = l.Parameter(genType(p.t), l.Name(p.n))
+
+  import m.IRBuilderAliases._
+
+  def genFun(params: List[Param], e: Exp): List[l.BasicBlock] =
+    m.execIRBuilder {
+      val name = l.Name("entry")
+      for {
+        _     <- m.emitBlockStart(name)
+        retOp <- genExp(e)
+        _     <- i.ret(retOp)
+        _     <- m.emitBlockStart(l.Name(""))
+      } yield ()
+    }
+
+  def genExp(exp: Exp): IRBuilder[l.Operand] = exp match {
+    case App(f, args) =>
+      for {
+        ops <- args.traverse { e =>
+          genExp(e)
+        }
+
+        result <- (f, ops) match {
+          case (Intrinsic.IAdd, List(a, b)) =>
+            i.add(a, b)
+
+        }
+      } yield (result)
+
+    case VInt(i) => c.int64[IRBuilder](i)
   }
 
-  def genParam(p: Param): l.Parameter = l.Parameter(genType(p.t), l.Name(p.n))
+  def genType(t: Type): l.Type = t match {
+    case TInt => l.IntegerType(64)
+  }
+
 }
