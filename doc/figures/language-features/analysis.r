f9cf2e9ed05df45ad0eaeb367159ed108d056d62
ian <ianmccall93@gmail.com>
2018-07-07 16:51:03 -0700

    Remove PCA in favour of MDS

--- doc/figures/language-features/analysis.r
+++ doc/figures/language-features/analysis.r
@@ -13,102 +13,12 @@ data.to.analyze = data # statically.typed.languages
 
 data.active = data.to.analyze[, -(0:1)]
 
-params.ncp = 5
-
-
-res.pca <- PCA(
-    data.active,
-    ncp = params.ncp,
-    graph = FALSE)
-
 plot.to.png = function(ptp.filename, ptp.function, ptp.width = 540, ptp.height = 540) {
   png(filename = ptp.filename, width = ptp.width, height = ptp.height)
   plot.result = ptp.function()
   print(plot.result)
   dev.off()
 }
-
-# plot the principal component weights
-plot.to.png("pca/principal-components.png",
-  function(){fviz_eig(res.pca, addlabels = TRUE)})
-
-gradient = c("#0099FF", "#FF4400")
-
-axes.primary = c(1, 2)
-axes.secondary = c(3, 2)
-
-# plot the language features
-plot.to.png("pca/language-features-primary-axes.png",
-    function() {
-      fviz_pca_var(
-        res.pca,
-        axes = axes.primary,
-        geom = c("text", "point"),
-        col.var = "cos2",
-        gradient.cols = gradient,
-        repel = TRUE)
-    }
-)
-
-plot.to.png("pca/language-features-secondary-axes.png",
-    function() {
-      fviz_pca_var(
-        res.pca,
-        axes = axes.secondary,
-        geom = c("text", "point"),
-        col.var = "cos2",
-        gradient.cols = gradient,
-        repel = TRUE)
-    }
-)
-
-# plot the languages
-plot.to.png("pca/languages-primary-axes.png",
-    function() {
-      fviz_pca_ind(
-        res.pca,
-        axes = axes.primary,
-        geom = c("text", "point"),
-        col.ind = "cos2",
-        gradient.cols = gradient,
-        repel = TRUE)
-    }
-)
-
-plot.to.png("pca/languages-secondary-axes.png",
-    function() {
-      fviz_pca_ind(
-        res.pca,
-        axes = axes.secondary,
-        geom = c("text", "point"),
-        col.ind = "cos2",
-        gradient.cols = gradient,
-        repel = TRUE)
-    }
-)
-
-# plot hierarchical clustering on principal components
-res.hcpc <- HCPC(res.pca, graph = FALSE)
-plot.to.png("pca/language-taxonomy.png",
-  function() {
-    fviz_dend(res.hcpc)
-  }
-)
-
-res.pca.transpose <- PCA(
-      t(data.active),
-      ncp = params.ncp,
-      graph = FALSE)
-
-res.hcpc.transpose <- HCPC(res.pca.transpose, graph = FALSE)
-plot.to.png("pca/language-feature-taxonomy.png",
-  function() {
-    fviz_dend(
-      cex = 0.9,
-      res.hcpc.transpose,
-      labels_track_height = 2.4)
-  }
-)
 
 plot.phylogram.to.png = function(file.name, title, data) {
 
