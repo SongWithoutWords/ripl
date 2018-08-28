31d614d2791a1d4f26fa818b334d9a57727c1ba7
ian <ianmccall93@gmail.com>
2018-08-28 09:37:56 -0700

    Light changes to untyped ast

--- reduce/src/Untyped/Ast.hs
+++ reduce/src/Untyped/Ast.hs
@@ -2,6 +2,11 @@ module Untyped.Ast where
 
 import Data.Map
 
+type NamedExps = Map String Exp
+
+newtype Ast = Ast NamedExps
+  deriving(Eq, Ord, Show)
+
 data Exp
   = App Exp [Exp]
   | If Exp Exp Exp
@@ -11,7 +16,7 @@ data Exp
   | Struct [(String, Exp)]
   | Constructor Exp
   | Name String
-  | Namespace (Map String Exp)
+  | Namespace NamedExps
   | VBln Bool
   | VInt Int
   | VStr String
